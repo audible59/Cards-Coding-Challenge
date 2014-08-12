d0861fe554dc85a8da9facc1cea1493b68e4d9d6
KevinERaffertyIIMagTek <administrator@administrators-MacBook-Pro.local>
2014-08-12 14:21:44 -0700

    Initial Commit

---
+++ CardsCodingChallenge/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CardsCodingChallenge
+//
+//  Created by Kevin E. Rafferty II on 8/12/14.
+//  Copyright (c) 2014 com.envoy.www. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CardsCodingChallenge/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CardsCodingChallenge
+//
+//  Created by Kevin E. Rafferty II on 8/12/14.
+//  Copyright (c) 2014 com.envoy.www. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
